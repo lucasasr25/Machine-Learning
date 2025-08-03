76890d2a189704ffccfdfb31b1284a7a9c5346fd
Lucas Araujo Silva dos Reis <araujo.luksss09@outlook.com>
2025-08-03 00:43:05 -0300

    updates

--- llms/softmax.py
+++ llms/softmax.py
@@ -1,10 +1,25 @@
-layer_outputs = [4.8, 1.21, 3.385]
+# layer_outputs = [4.8, 1.21, 3.385]
 
-E = 2.71
+# E = 2.71
 
-exp_values = []
+# exp_values = []
 
-for output in layer_outputs:
-    exp_values.append(E ** output)
+# for output in layer_outputs:
+#     exp_values.append(E ** output)
 
-print(exp_values)
+# print(exp_values)
+
+
+import numpy as np
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        # Evitar overflow (fazendo cada item do array menos o maior valor do array, depois E ** i) 
+        exp_values = np.exp(inputs -  np.max(inputs, axis = 1, keepdims=True))
+        
+        # Aqui é feito o valor da probabilidade para cada input
+        probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims=True)
+        self.output = probabilities
+
+
