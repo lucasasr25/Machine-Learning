86d13dc8abd8c84801e715dbe2db00f534c77968
Lucas Araujo Silva dos Reis <araujo.luksss09@outlook.com>
2025-08-03 17:58:09 -0300

    Update

---
+++ llms/cross_entropy.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Loss_CategoricalCrossentropy(Loss):
+
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+
+        #soma valores de décimo para evitar log 0 e log 1
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1: 
+            correct_confidences = y_pred_clipped[ 
+                range(samples), 
+                y_true 
+            ] 
+
+        elif len(y_true.shape) == 2: 
+            correct_confidences = np.sum( 
+                y_pred_clipped * y_true, 
+                axis=1
+            ) 
+
+        negative_log_likelihoods = -np.log(correct_confidences) 
+        return negative_log_likelihoods
